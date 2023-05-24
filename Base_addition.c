fb6c3b3cda5cd19e4fb01a86368e76d7e73cf79d
N L Chandana Lakshmi Priya <127175407+NLChandana@users.noreply.github.com>
2023-05-24 14:34:15 +0530

    Created Base_addition.c

---
+++ Base_addition.c
@@ -0,0 +1,54 @@
+/*
+i/p: num1=101010
+     num2=110101
+     base=2
+o/p: 1011111
+
+i/p: num1=123
+     num2=12
+     base=4
+o/p: 201
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,base,sum,carry=0,res=0,t1,t2,place=1;
+    scanf("%d%d%d",&num1,&num2,&base);
+    while(num1!=0 || num2!=0)
+    {
+        t1=num1%10;
+        t2=num2%10;
+        sum=(t1+t2)+carry;
+        res=res+((sum%base)*place);
+        
+        num1=num1/10;
+        num2=num2/10;
+        carry=sum/base;
+        place*=10;
+    }
+
+    printf("%d",carry*place+res); 
+}
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2,base,sum,carry=0,res=0,t1,t2,place=1;
+    scanf("%d%d%d",&num1,&num2,&base);
+    while((num1/place) || (num2/place))
+    {
+      
+        sum=(((num1/place)%10)+((num2/place)%10))+carry;
+        res=res+((sum%base)*place);
+        carry=sum/base;
+        place*=10;
+    }
+
+    printf("%d",carry*place+res); 
+}
+
